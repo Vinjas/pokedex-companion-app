852f63015e86b570adbf9e4d3143be1a855243e8
vinjas <danielvinyastorres@gmail.com>
2021-06-06 16:18:00 +0200

    Style More button to pokecards

--- src/API/next-pokemons.js
+++
@@ -1,7 +0,0 @@
-const URL = 'https://pokeapi.co/api/v2/pokemon';
-
-/*?offset=${}&limit=20*/
-
-export const getMorePokemons = (offset) => {
-  return fetch(`${URL}?offset=${offset}&limit=20`).then((response) => response.json());
-};
