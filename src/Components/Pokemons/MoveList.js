91de74d093a14af48d791a0f664d05eec20365ec
vinjas <danielvinyastorres@gmail.com>
2021-06-15 19:05:55 +0200

    Add submenu for moves and refine margins

--- src/Components/Pokemons/MoveList.js
+++ src/Components/Pokemons/MoveList.js
@@ -15,6 +15,9 @@ const MoveList = (props) => {
     const eggMoves = props.moves.filter(elem => {
         return elem.version_group_details[0].move_learn_method.name === "egg"
     })
+    const tutorMoves = props.moves.filter(elem => {
+        return elem.version_group_details[0].move_learn_method.name === "tutor"
+    })
 
 
     function lvlLearned() {
@@ -30,35 +33,41 @@ const MoveList = (props) => {
     return (
         <div>            
             
-            <div className="pokemon__menu ">
+            <div className="movesCard__menu ">
                 <div 
                 className={classNames({
-                    pokemon__title: true,
-                    "pokemon__title--menu": (submenu != 1),
-                    "pokemon__title--active": (submenu === 1)
+                    "movesCard__menu--inactive": (submenu != 1),
+                    "movesCard__menu--active": (submenu === 1)
                 })} 
                 onClick={() => setSubMenu(1)}>
-                    Level-Up
+                    Level
                 </div>
 
                 <div 
                 className={classNames({
-                    pokemon__title: true,
-                    "pokemon__title--menu": (submenu != 2),
-                    "pokemon__title--active": (submenu === 2)
+                    "movesCard__menu--inactive": (submenu != 2),
+                    "movesCard__menu--active": (submenu === 2)
                 })}           
                 onClick={() => setSubMenu(2)}>
-                    Machine
+                    TM/HM
                 </div>
 
                 <div 
                 className={classNames({
-                    pokemon__title: true,
-                    "pokemon__title--menu": (submenu != 3),
-                    "pokemon__title--active": (submenu === 3)
+                    "movesCard__menu--inactive": (submenu != 3),
+                    "movesCard__menu--active": (submenu === 3)
                 })}           
                 onClick={() => setSubMenu(3)}>
                     Egg
+                </div>
+
+                <div 
+                className={classNames({
+                    "movesCard__menu--inactive": (submenu != 4),
+                    "movesCard__menu--active": (submenu === 4)
+                })}           
+                onClick={() => setSubMenu(4)}>
+                    Tutor
                 </div>
             </div>
 
@@ -68,7 +77,7 @@ const MoveList = (props) => {
                 levelMoves.map((elem) => {
                     return (            
                         <MoveCard 
-                            key={props.id }{...elem}
+                            key={elem.move.url.match(/\b(\d+)/g)} {...elem}
                             moveName={elem.move && elem.move.name}
                             moveId={elem.move && elem.move.url.match(/\b(\d+)/g)}
                             level={elem.version_group_details && elem.version_group_details[0].level_learned_at}   
@@ -98,6 +107,22 @@ const MoveList = (props) => {
             <div>
             {submenu === 3 && 
                 eggMoves.map((elem) => {
+                    return (            
+                        <MoveCard 
+                            key={props.id }{...elem}
+                            moveName={elem.move && elem.move.name}
+                            moveId={elem.move && elem.move.url.match(/\b(\d+)/g)}
+                            level={elem.version_group_details && elem.version_group_details[0].level_learned_at}   
+                        />
+                    )
+                    }
+                )
+            }
+            </div>
+
+            <div>
+            {submenu === 4 && 
+                tutorMoves.map((elem) => {
                     return (            
                         <MoveCard 
                             key={props.id }{...elem}
--- src/Components/Pokemons/Stats.js
+++ src/Components/Pokemons/Stats.js
@@ -6,17 +6,36 @@ const Stats = (props) => {
         <div>
             {props.pokemon.stats &&
             props.pokemon.stats.map((stat, index) => {
+                var nameMove = stat['stat']['name']
+                if(stat['stat']['name'] === "special-attack") {
+                    nameMove = 'Sp. Atk'
+                } else if (stat['stat']['name'] === "special-defense") {
+                    nameMove = 'Sp. Def'
+                }
+
                 return (
-                    <div className="stats">
-                        <div key={index} className="stats__container">
-                            <div className="pokemon__title">{stat['stat']['name']}</div>
-                            <div className="pokemon__title pokemon__title--strong">{stat.base_stat}</div>
+                    <div key={`${stat}${index}`} className="stats">
+                        <div key={`${stat}${index}`} className="stats__container">
+                            <div 
+                            key={stat['stat']['name']} 
+                            className="pokemon__title"
+                            style={{fontWeight:400}}>                           
+                                {nameMove}
+                            </div>
+
+                            <div 
+                            key={stat.base_stat} 
+                            className="pokemon__title pokemon__title--strong">
+                                {stat.base_stat}
+                            </div>
                         </div>
                     </div>
                 );
             })}
             <div className="stats__container">
-                <div className="pokemon__title">Total</div>
+                <div className="pokemon__title" style={{fontWeight:400}}>
+                    Total
+                </div>
                 <div className="pokemon__title pokemon__title--strong">
                     {props.pokemon.stats && props.pokemon.stats.reduce((acc, elem) => {                      
                         return acc + elem.base_stat
